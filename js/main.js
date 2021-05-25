2a524bf379b5ed14b24c2a74c97c4f55a6d86f9f
Daniel Huang <32477164+danielnz2017@users.noreply.github.com>
2021-05-25 23:38:03 +1200

    updated
    Added a little help tool and made a mall fix to some styles.

--- js/main.js
+++ js/main.js
@@ -14,3 +14,15 @@ function highlight_word(searchpara) {
     }
 }
 
+
+
+
+
+// function search(query){
+//     query.preventDefault();
+//     let vquery = query.target.elements["search"].value;
+//     console.log(vquery)
+    
+//  }
+ 
+//  something.addEventListener("submit", search, false)
